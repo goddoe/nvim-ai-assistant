cbd7298aa067252ef5976b0a3fa8fb8ed4577713
Sung-ju.Kim <goddoe2@gmail.com>
2023-05-13 22:29:50 +0900

    fix get message num

--- lua/nvim-ai-assistant/commands.lua
+++ lua/nvim-ai-assistant/commands.lua
@@ -2,4 +2,4 @@ vim.cmd([[command! -range=% -nargs=1 NaaAskVisual lua require('nvim-ai-assistant
 vim.cmd([[command! -nargs=1 NaaAsk lua require('nvim-ai-assistant').call_llm(<f-args>)]])
 vim.cmd([[command! NaaResetMsgs lua require('nvim-ai-assistant').reset_message_list()]])
 vim.cmd([[command! NaaGetMsgs lua require('nvim-ai-assistant').get_curr_message_list()]])
-vim.cmd([[command! NaaGetNumMsgs lua require('nvim-ai-assistant').get_message_num()]])
+vim.cmd([[command! NaaGetNumMsgs lua require('nvim-ai-assistant').get_curr_message_num()]])
