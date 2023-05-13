e3a2842eefa4babd23595168f7504cc743ac2459
Sung-ju.Kim <goddoe2@gmail.com>
2023-05-13 20:17:39 +0900

    restore setup

--- lua/nvim-ai-assistant/init.lua
+++ lua/nvim-ai-assistant/init.lua
@@ -170,14 +170,14 @@ local function call_llm_visual(start_line, end_line, query)
     call_llm(text)
 end
 
--- local function setup()
---   require("nvim-ai-assistant.keymap")
---   require("nvim-ai-assistant.commands")
--- end
+local function setup()
+  require("nvim-ai-assistant.keymap")
+  require("nvim-ai-assistant.commands")
+end
 
 M.call_llm_visual = call_llm_visual
 M.call_llm = call_llm
--- M.setup = setup
+M.setup = setup
 
 -- vim.cmd([[command! -range=% -nargs=1 AskToLLMVisual lua require('nvim-ai-assistant').call_llm_visual(<line1>, <line2>, <f-args>)]])
 -- vim.cmd([[command! -nargs=1 AskToLLM lua require('nvim-ai-assistant').call_llm(<f-args>)]])
